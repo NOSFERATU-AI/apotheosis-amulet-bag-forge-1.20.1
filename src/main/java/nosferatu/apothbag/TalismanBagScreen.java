f223beb001640caea589f6b98ae88785ce6b6795
NOSFERATU-AI <nosferatu.ai01@gmail.com>
2026-07-05 21:41:31 +0200

    Use vanilla container background

--- src/main/java/nosferatu/apothbag/TalismanBagScreen.java
+++ src/main/java/nosferatu/apothbag/TalismanBagScreen.java
@@ -3,13 +3,18 @@ package nosferatu.apothbag;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
 public class TalismanBagScreen extends AbstractContainerScreen<TalismanBagMenu> {
+    private static final ResourceLocation CONTAINER_TEXTURE =
+            new ResourceLocation("textures/gui/container/generic_54.png");
+    private static final int BAG_ROWS = 1;
+
     public TalismanBagScreen(TalismanBagMenu menu, Inventory inventory, Component title) {
         super(menu, inventory, title);
         this.imageWidth = 176;
-        this.imageHeight = 133;
+        this.imageHeight = 114 + BAG_ROWS * 18;
         this.inventoryLabelY = this.imageHeight - 94;
     }
 
@@ -24,29 +29,9 @@ public class TalismanBagScreen extends AbstractContainerScreen<TalismanBagMenu> 
     protected void renderBg(GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
         int x = (this.width - this.imageWidth) / 2;
         int y = (this.height - this.imageHeight) / 2;
+        int topHeight = BAG_ROWS * 18 + 17;
 
-        graphics.fill(x, y, x + this.imageWidth, y + this.imageHeight, 0xFFC6C6C6);
-        graphics.fill(x + 3, y + 3, x + this.imageWidth - 3, y + this.imageHeight - 3, 0xFF8B8B8B);
-        graphics.fill(x + 6, y + 6, x + this.imageWidth - 6, y + this.imageHeight - 6, 0xFFC6C6C6);
-
-        for (int i = 0; i < TalismanBagItem.MAX_SLOTS; i++) {
-            drawSlot(graphics, x + 43 + i * 18, y + 19);
-        }
-
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 9; col++) {
-                drawSlot(graphics, x + 7 + col * 18, y + 50 + row * 18);
-            }
-        }
-
-        for (int col = 0; col < 9; col++) {
-            drawSlot(graphics, x + 7 + col * 18, y + 108);
-        }
-    }
-
-    private void drawSlot(GuiGraphics graphics, int x, int y) {
-        graphics.fill(x, y, x + 18, y + 18, 0xFF555555);
-        graphics.fill(x + 1, y + 1, x + 17, y + 17, 0xFFE0E0E0);
-        graphics.fill(x + 2, y + 2, x + 16, y + 16, 0xFF8B8B8B);
+        graphics.blit(CONTAINER_TEXTURE, x, y, 0, 0, this.imageWidth, topHeight);
+        graphics.blit(CONTAINER_TEXTURE, x, y + topHeight, 0, 126, this.imageWidth, 96);
     }
 }
