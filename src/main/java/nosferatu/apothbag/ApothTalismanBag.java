2dbbd9ab05c87d9a00dc6f63502d458f5e9931b7
NOSFERATU-AI <nosferatu.ai01@gmail.com>
2026-07-05 21:19:16 +0200

    Use official Forge 1.20.1 mappings

--- src/main/java/nosferatu/apothbag/ApothTalismanBag.java
+++ src/main/java/nosferatu/apothbag/ApothTalismanBag.java
@@ -1,7 +1,10 @@
 package nosferatu.apothbag;
 
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -14,16 +17,22 @@ public class ApothTalismanBag {
     public static final String MODID = "apoth_talisman_bag";
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static final DeferredRegister<MenuType<?>> MENUS = DeferredRegister.create(ForgeRegistries.MENU_TYPES, MODID);
 
     public static final RegistryObject<Item> TALISMAN_BAG = ITEMS.register("talisman_bag",
-            () -> new TalismanBagItem(new Item.Properties().m_41487_(1)));
+            () -> new TalismanBagItem(new Item.Properties().stacksTo(1)));
 
     public static final RegistryObject<Item> LOCKED_SLOT = ITEMS.register("locked_slot",
-            () -> new Item(new Item.Properties().m_41487_(1)));
+            () -> new Item(new Item.Properties().stacksTo(1)));
+
+    public static final RegistryObject<MenuType<TalismanBagMenu>> TALISMAN_BAG_MENU = MENUS.register("talisman_bag",
+            () -> IForgeMenuType.create((windowId, inventory, data) ->
+                    new TalismanBagMenu(windowId, inventory, new TalismanBagContainer(ItemStack.EMPTY))));
 
     public ApothTalismanBag() {
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
         ITEMS.register(modBus);
+        MENUS.register(modBus);
         MinecraftForge.EVENT_BUS.addListener(BagEvents::onPlayerTick);
     }
 }
